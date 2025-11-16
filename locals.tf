96f14b8c5983204321737db76327557ecd760530
chakradhar06 <chakradhar06@gmail.com>
2025-11-16 00:49:45 -0500

    u

--- locals.tf
+++ locals.tf
@@ -4,12 +4,10 @@ locals {
     private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_id.value)
     public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_id.value)[0]
     public_subnet_ids = split(",",data.aws_ssm_parameter.public_subnet_id.value)
-    subnet_ids = "${var.component}" == "frontend" ? public_subnet_ids : private_subnet_ids
-
+    subnet_ids = "${var.component}" == "frontend" ? local.public_subnet_ids : local.private_subnet_ids
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn
-    alb_listener_arn = "${var.component}" == "frontend" ? frontend_alb_listener_arn : backend_alb_listener_arn
-    
+    alb_listener_arn = "${var.component}" == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
     host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}-backend-alb-${var.environment}.${var.domain_name}"
     tg_port = "${var.component}" == "frontend" ? 80 : 8080
     vpc_id = data.aws_ssm_parameter.vpc_id.value
